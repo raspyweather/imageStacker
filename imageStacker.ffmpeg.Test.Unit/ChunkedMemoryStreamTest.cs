b3cfa1b05d689e2c31abe4e59906b90ae100f1f9
raspyweather <raspyweather@gmail.com>
2020-10-03 20:03:50 +0200

    added tests for ChunkedStreams

---
+++ imageStacker.ffmpeg.Test.Unit/ChunkedMemoryStreamTest.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Concurrent;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using Xunit;
+
+namespace imageStacker.ffmpeg.Test.Unit
+{
+    public class ChunkedMemoryStreamTest
+    {
+        [Fact]
+        public void EmitsImageOnOverflow()
+        {
+            var (chunkedStream, bytes, concurrentQueue) = Prepare(64, 64);
+
+            chunkedStream.Write(bytes, 0, bytes.Length);
+
+            Assert.Single(concurrentQueue);
+        }
+
+        [Fact]
+        public void IsInCleanStateAfterWrite()
+        {
+            var (chunkedStream, bytes, _) = Prepare(64, 64);
+
+            chunkedStream.Write(bytes, 0, bytes.Length);
+
+            Assert.False(chunkedStream.HasUnwrittenData);
+        }
+
+        [Fact]
+        public void IsDi6rtyAfterIncompleteImageWrite()
+        {
+            var (chunkedStream, bytes, _) = Prepare(48, 64);
+
+            chunkedStream.Write(bytes, 0, bytes.Length);
+
+            Assert.True(chunkedStream.HasUnwrittenData);
+        }
+
+        [Fact]
+        public void LargerChunksWrittenIntoSmallerImages()
+        {
+            var (chunkedStream, bytes, concurrentQueue) = Prepare(640, 160);
+
+            chunkedStream.Write(bytes);
+
+            Assert.Equal(4, concurrentQueue.Count);
+            Assert.False(chunkedStream.HasUnwrittenData);
+        }
+
+        [Fact]
+        public void ChunkOverflowsIntoMultipleImages()
+        {
+            var (chunkedStream, bytes, concurrentQueue) = Prepare(48, 64);
+
+            for (int i = 0; i < 10; i++)
+            {
+                chunkedStream.Write(bytes, 0, bytes.Length);
+            }
+
+            Assert.Equal(7, concurrentQueue.Count);
+            Assert.True(chunkedStream.HasUnwrittenData);
+        }
+
+        private (ChunkedMemoryStream chunkedStream, byte[] bytes, ConcurrentQueue<byte[]> concurrentQueue) Prepare(int bytesData, int bytesPerChunk)
+        {
+            var concurrentQueue = new ConcurrentQueue<byte[]>();
+            var chunkedStream = new ChunkedMemoryStream(bytesPerChunk, concurrentQueue);
+
+            var bytes = new byte[bytesData];
+            var random = new Random();
+            random.NextBytes(bytes);
+            return (chunkedStream, bytes, concurrentQueue);
+        }
+
+    }
+}
