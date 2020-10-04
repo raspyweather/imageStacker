c6620827c48c8ed232c580eac4896e6f6afbe256
raspyweather <raspyweather@gmail.com>
2020-10-04 12:14:40 +0200

    added Ffmpeg argument to allow for lower framerate

---
+++ imageStacker.ffmpeg/FfmpegFpsArgument.cs
@@ -0,0 +1,16 @@
+﻿using FFMpegCore.Arguments;
+
+namespace imageStacker.ffmpeg
+{
+    internal class FfmpegFpsArgument : IArgument
+    {
+        public FfmpegFpsArgument(double fps)
+        {
+            Fps = fps;
+        }
+
+        public double Fps { get; set; }
+
+        public string Text => $"-vf fps=\"{Fps}\"";
+    }
+}
---
+++ imageStacker.ffmpeg/FfmpegVideoReaderArguments.cs
@@ -0,0 +1,11 @@
+﻿namespace imageStacker.ffmpeg
+{
+    public class FfmpegVideoReaderArguments
+    {
+        public double Framerate { get; set; }
+
+        public string PathToFfmpeg { get; set; }
+
+        public string[] InputFiles { get; set; }
+    }
+}
