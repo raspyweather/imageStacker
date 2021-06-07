03f4c1dfae0b770ebb45fa4b405a18466b2f3569
Armin Beck <Armin.Beck@camao.one>
2021-06-08 01:13:20 +0200

    added tests for FfmpegVideoReader

---
+++ imageStacker.ffmpeg.Test.Unit/FfmpegVideoReaderTest.cs
@@ -0,0 +1,27 @@
+﻿using imageStacker.Core;
+using imageStacker.Core.ByteImage;
+using imageStacker.Core.Test.Unit.ByteImage;
+using System.IO;
+
+namespace imageStacker.ffmpeg.Test.Unit
+{
+    public class FfmpegVideoReaderTest : VideoReaderTestBase
+    {
+        public FfmpegVideoReaderTest() : base(
+            new MutableByteImageProvider(8, 8),
+            new MutableByteImageFactory(new Logger(TextWriter.Null)),
+            255)
+        {
+        }
+        protected override IImageReader<MutableByteImage> Reader =>
+            new FfmpegVideoReader(
+                new FfmpegVideoReaderArguments
+                {
+                    InputFile = TempFile,
+                    CustomArgs = "-crf 0 -qp 0"
+                },
+                factory,
+                new Logger(TextWriter.Null));
+
+    }
+}
---
+++ imageStacker.ffmpeg.Test.Unit/VideoReaderTestBase.cs
@@ -0,0 +1,52 @@
+﻿using imageStacker.Core;
+using imageStacker.Core.ByteImage;
+using imageStacker.Core.Test.Unit.ByteImage;
+using imageStacker.Core.Test.Unit.Readers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imageStacker.ffmpeg.Test.Unit
+{
+    public abstract class VideoReaderTestBase : OrderedReaderTestBase
+    {
+        public VideoReaderTestBase(IImageProvider<MutableByteImage> imageProvider, IMutableImageFactory<MutableByteImage> factory, int imagesCount)
+            : base(imageProvider, factory, imagesCount)
+        {
+        }
+
+        protected string TempFile = "";
+
+        protected override void Prepare()
+        {
+            base.Prepare();
+
+            TempFile = Path.Combine(tempPath, Path.GetRandomFileName() + ".apng");
+
+            var args = FFMpegCore.FFMpegArguments
+               .FromFileInput(Path.Combine(tempPath, "%05d.png"), false, args =>
+                args.WithFramerate(1)
+                     .ForcePixelFormat("rgb24")
+                     .WithFramerate(1))
+                .OutputToFile(TempFile, true, args =>
+                    args.WithSpeedPreset(FFMpegCore.Enums.Speed.VerySlow)
+                        //.WithFramerate(1.0)
+                        //.WithConstantRateFactor(0)
+                        .ForcePixelFormat("rgb24")
+                );
+            try
+            {
+                args.ProcessSynchronously(true);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
